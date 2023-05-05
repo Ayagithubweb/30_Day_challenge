9fd679792d9ed2258f49ccc62520e061675162fc
Ayagithubweb <ayaabdelmon@gmail.com>
2023-05-05 09:40:41 +0300

    Create flutter app

---
+++ app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+            child: Text(
+          "30 Day Challenge....💪",
+          style: Theme.of(context).textTheme.titleMedium,
+        )),
+      ),
+    );
+  }
+}
