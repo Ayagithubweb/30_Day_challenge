98b94b73120678b48531a49883cdcc7ed571accb
Ayagithubweb <ayaabdelmon@gmail.com>
2023-07-05 21:13:44 +0300

    Add Day1

---
+++ app/lib/July/Day1/day_1.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:app/July/Day1/constants.dart';
+
+class Day1 extends StatelessWidget {
+  const Day1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Food Ordering App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primaryColor: kPrimaryColor,
+            scaffoldBackgroundColor: Colors.white,
+            textTheme: const TextTheme(
+              bodyText1: TextStyle(color: ksecondaryColor),
+              bodyText2: TextStyle(color: ksecondaryColor),
+            )),
+        home: Scaffold(
+          body: Center(
+            child: Text(
+              "JulyDays",
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+          ),
+        ));
+  }
+}
