9a93afd75e82e75aa9337d0b9491ea973a8ca5c8
Ayagithubweb <ayaabdelmon@gmail.com>
2023-07-11 21:49:45 +0300

    ...

--- app/lib/July/Day8/screens/home_screen.dart
+++ app/lib/July/Day8/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:app/July/Day8/models/chat_model.dart';
+import 'package:app/July/Day8/models/message_model.dart';
 import 'package:app/July/Day8/models/user.dart';
 import 'package:app/July/Day8/widgets/custom_appbar.dart';
 import 'package:app/July/Day8/widgets/users.dart';
@@ -53,16 +54,27 @@ class CustomContainer extends StatelessWidget {
             topRight: Radius.circular(50),
           )),
       child: ListView.separated(
-        separatorBuilder: (context, index) => const SizedBox(height: 8),
-        itemCount: Chat.chats.length,
-        itemBuilder: (context, index) => ListTile(
-          leading: CircleAvatar(
-              radius: 32,
-              backgroundImage: NetworkImage(User.users[0].imageUrl)),
-          title: Text(Chat.chats[index].users.toString()),
-          subtitle: Text(Chat.chats[index].messages.toString()),
-        ),
-      ),
+          separatorBuilder: (context, index) => const SizedBox(height: 8),
+          itemCount: Chat.chats.length,
+          itemBuilder: (context, index) {
+            List<Chat> chats = Chat.chats;
+            User user =
+                chats[index].users!.where((user) => user.id != '1').first;
+
+            chats[index]
+                .messages!
+                .sort((a, b) => b.createdAt.compareTo(a.createdAt));
+            Message lastMessages = chats[index].messages!.first;
+
+            ListTile(
+              leading: CircleAvatar(
+                  radius: 32, backgroundImage: NetworkImage(user.imageUrl)),
+              title: Text("${user.name} ${user.surname}"),
+              subtitle: Text(lastMessages.text),
+              trailing: Text(
+                  '${lastMessages.createdAt.hour}:${lastMessages.createdAt.day}'),
+            );
+          }),
     );
   }
 }
